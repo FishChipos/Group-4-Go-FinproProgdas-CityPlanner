cea9b089d3c3967d05a6ddf9df5317a8a9486cf0
Rivenoxern <sakabudi.m@gmail.com>
2025-05-26 22:30:47 +0700

    cityevaluation.c

--- src/pages/cityevaluation.c
+++ src/pages/cityevaluation.c
@@ -15,9 +15,9 @@ int calculateDensity(float area, unsigned long long population) {
 
     float density = population / area;
 
-    if (density <= 1000) return 100;
-    else if (density <= 3000) return 75;
-    else if (density <= 5000) return 50;
+    if (density <= 5000) return 100;
+    else if (density <= 8000) return 75;
+    else if (density <= 10000) return 50;
     else return 25;
 }
 

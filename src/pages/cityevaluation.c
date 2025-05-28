607392743b8cecc452f5185238fb0a9f7634b95d
Rivenoxern <sakabudi.m@gmail.com>
2025-05-28 14:22:13 +0700

    Update cityevaluation.c

--- src/pages/cityevaluation.c
+++ src/pages/cityevaluation.c
@@ -12,12 +12,34 @@ void pageCityEvaluation(App *app, City *city) {
     bool pageShouldClose = false;
 
     while (!pageShouldClose) {
-        printf("\nCity Evaluation\n");
+        enum {
+            BACK = 1;
+        } choice = -1;
 
+        system("clear");
+    
+        printf("\nCity Evaluation\n");
+        puts("");
+    
         int score = calculateDensity(city->area, city->population);
 
         printf("City Density: %d\n", score);
+        puts("");
+        puts("1. Back\n");
 
-        }    
+        printf("%s", "Choice: ");
+        char buffer[128];
+        fgets(buffer, 128, stdin);
+        sscanf(buffer, "%d", (int*)&choice);
 
-    }
+        puts("");
+        switch (choice) {
+            case BACK:
+                pageShouldClose = true;
+                break;
+            default:
+                promptInvalidInput();
+                break;
+        }
+    }    
+}

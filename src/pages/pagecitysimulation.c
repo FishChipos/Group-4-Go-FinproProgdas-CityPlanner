2ccc2e5b3cf98e48ca49c9abfc3aec834f2a4f63
Carlsson Khovis <carlkho99@gmail.com>
2025-05-29 09:39:14 +0700

    Cleaned up include order

--- src/pages/pagecitysimulation.c
+++ src/pages/pagecitysimulation.c
@@ -1,8 +1,10 @@
+#include "pages.h"
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#include "pages.h"
+
 #include "../citysimulation.h"
 
 void pageCitySimulation(App *app, City *city) {
